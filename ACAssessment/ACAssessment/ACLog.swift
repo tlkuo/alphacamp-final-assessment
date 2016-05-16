b955e608d173833f520badf694ba5bfefae46e0f
martin <tlkuo.frb@gmail.com>
2016-05-16 15:12:10 +0800

    Add layout for observing view controller life cycle

---
+++ ACAssessment/ACAssessment/ACLog.swift
@@ -0,0 +1,27 @@
+//
+//  ACLog.swift
+//  ACAssessment
+//
+//  Created by martin on 2016/5/16.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import Foundation
+
+class ACLog {
+    static let sharedInstance: ACLog = ACLog()
+
+    private var logs: [String] = []
+
+    private init() {
+
+    }
+
+    func log(message: String) {
+        logs.append("\(NSDate()) - \(message)")
+    }
+
+    func dump() -> String {
+        return logs.joinWithSeparator("\n")
+    }
+}
