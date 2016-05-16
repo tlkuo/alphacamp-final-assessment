cdd33c8c9c2276f0f2d2ea9deb798fa919d1b6d1
martin <tlkuo.frb@gmail.com>
2016-05-16 18:54:57 +0800

    Add data for table view and alert content

--- ACAssessment/ACAssessment/AlertViewController.swift
+++ ACAssessment/ACAssessment/AlertViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class AlertViewController: UIViewController {
+    
+    var alertData: [String] = ["data 1", "data 2", "data 3", "data 4", "data 5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,12 +39,14 @@ class AlertViewController: UIViewController {
 extension AlertViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return alertData.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("AlertCell", forIndexPath: indexPath)
-        
+
+        cell.textLabel?.text = alertData[indexPath.row]
+
         return cell
     }
 }
