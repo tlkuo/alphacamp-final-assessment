7be4fd83074acd57539376b8fc1467a88f0cf660
martin <tlkuo.frb@gmail.com>
2016-05-16 19:41:42 +0800

    Add layout for TableView & Camera

---
+++ ACAssessment/ACAssessment/ImageListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ImageListViewController.swift
+//  ACAssessment
+//
+//  Created by martin on 2016/5/16.
+//  Copyright © 2016年 Frainbow. All rights reserved.
+//
+
+import UIKit
+
+class ImageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
+
+extension ImageListViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ImageItem", forIndexPath: indexPath)
+        
+        return cell
+    }
+}
